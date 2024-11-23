81789e9bb1182c6d676417f4a52879b884ce9178
Yuri <Jolycougar@gmail.com>
2024-11-23 16:26:14 +0300

    add: tests view books, change status library file and book

--- tests.py
+++ tests.py
@@ -3,7 +3,32 @@ import os
 import json
 from unittest import mock
 from library import Library
-from models import Book
+from models import Book, LibraryFile
+
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        """ Создание экземпляра книги для тестов."""
+        self.book = Book(id=1, title="Test Title", author="Test Author", year=2023, status="В наличии")
+
+    def test_initialization(self):
+        """ Тестирование инициализации книги. """
+        self.assertEqual(self.book.id, 1)
+        self.assertEqual(self.book.title, "Test Title")
+        self.assertEqual(self.book.author, "Test Author")
+        self.assertEqual(self.book.year, 2023)
+        self.assertEqual(self.book.status, "В наличии")
+
+    def test_to_dict(self):
+        """ Тестирование метода to_dict."""
+        expected_dict = {
+            "id": 1,
+            "title": "Test Title",
+            "author": "Test Author",
+            "year": 2023,
+            "status": "В наличии"
+        }
+        self.assertEqual(self.book.to_dict(), expected_dict)
 
 
 class TestLibrary(unittest.TestCase):
@@ -79,3 +104,87 @@ class TestLibrary(unittest.TestCase):
         with mock.patch('builtins.print') as mocked_print:
             self.library.search_book("Неизвестная книга")
             mocked_print.assert_called_once_with('В библиотеке нет не одной книги')
+
+    def test_view_books(self):
+        """ Тестирование просмотра всех книг в библиотеке. """
+        self.library.add_book("Test_title_1", "Test_author_1", 2021)
+        self.library.add_book("Test_title_2", "Test_author_2", 2022)
+
+        with mock.patch('builtins.print') as mocked_print:
+            self.library.view_books()
+            mocked_print.assert_any_call("Книга ID: 1,\n Автор: Test_author_1,\n Название: Test_title_1,\n "
+                                         "Год выпуска: 2021,\n Статус: В наличии\n")
+            mocked_print.assert_any_call("Книга ID: 2,\n Автор: Test_author_2,\n Название: Test_title_2,\n "
+                                         "Год выпуска: 2022,\n Статус: В наличии\n")
+
+    def test_view_books_with_empty_library(self):
+        """ Тестирование просмотра всех книг в пустой библиотеке. """
+        with mock.patch('builtins.print') as mocked_print:
+            self.library.view_books()
+            mocked_print.assert_any_call("Библиотека пустая, Добавьте новые книги!")
+
+    def test_change_status(self):
+        """ Тестирование изменения статуса книги. """
+        self.library.add_book("Test_title", "Test_author", 2024)
+        self.library.change_status(1)
+        book = self.library.library.lib()[1]
+        self.assertEqual(book.status, "Выдана")
+
+        self.library.change_status(1)
+        book = self.library.library.lib()[1]
+        self.assertEqual(book.status, "В наличии")
+
+    def test_change_status_nonexistent_book(self):
+        """ Тестирование изменения статуса несуществующей книги. """
+        with mock.patch('builtins.print') as mocked_print:
+            self.library.change_status(999)
+            mocked_print.assert_called_once_with("Книги с таким ID не найдено проверьте ID и попробуйте снова")
+
+
+class TestLibraryFile(unittest.TestCase):
+    def setUp(self):
+        """ Создание экземпляра библиотеки для тестов. """
+        self.test_filename = "test_library.json"
+        self.library_file = LibraryFile(filename=self.test_filename)
+        self.library_file.library = {}
+
+    def tearDown(self):
+        """ Удаление тестового файла после тестов. """
+        if os.path.exists(self.test_filename):
+            os.remove(self.test_filename)
+
+    def test_load_books(self):
+        """ Тестирование загрузки книг из файла JSON. """
+        test_data = {
+            "1": {"id": "1", "title": "Test Title", "author": "Test Author", "year": 2023, "status": "В наличии"}
+        }
+        with open(self.test_filename, mode="w", encoding="utf-8") as file:
+            json.dump(test_data, file, ensure_ascii=False, indent=4)
+
+        library_file = LibraryFile(filename=self.test_filename)
+        self.assertEqual(len(library_file.lib()), 1)
+        book = library_file.lib()[1]
+        self.assertEqual(book.title, "Test Title")
+        self.assertEqual(book.author, "Test Author")
+        self.assertEqual(book.year, 2023)
+        self.assertEqual(book.status, "В наличии")
+
+    def test_save_books(self):
+        """ Тестирование сохранения книг в файл JSON. """
+        self.library_file.library[1] = Book(1, "Test Title", "Test Author", 2023, "В наличии")
+        self.library_file.save_books()
+
+        with open(self.test_filename, mode="r", encoding="utf-8") as file:
+            data = json.load(file)
+            self.assertIn("1", data)
+            self.assertEqual(data["1"]["title"], "Test Title")
+            self.assertEqual(data["1"]["author"], "Test Author")
+            self.assertEqual(data["1"]["year"], 2023)
+            self.assertEqual(data["1"]["status"], "В наличии")
+
+    def test_lib(self):
+        """ Тестирование метода lib. """
+        self.library_file.library[1] = Book(1, "Test Title", "Test Author", 2023, "В наличии")
+        self.assertEqual(len(self.library_file.lib()), 1)
+        self.assertEqual(self.library_file.lib()[1].title, "Test Title")
+        self.assertEqual(self.library_file.lib()[1].author, "Test Author")
